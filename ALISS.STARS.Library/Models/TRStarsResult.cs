cbcfb365b1457980026797ecb69d14d12f713c80
JetB14ck <116273550+JetB14ck@users.noreply.github.com>
2023-06-22 11:40:44 +0700

    jet : update fixed bug save stars personal report error

--- ALISS.STARS.Library/Models/TRStarsResult.cs
+++ ALISS.STARS.Library/Models/TRStarsResult.cs
@@ -50,6 +50,6 @@ namespace ALISS.STARS.Library.Models
         public string srr_updateuser { get; set; }
         public DateTime? srr_updatedate { get; set; }
         public string srr_remark { get; set; }
-        public Boolean srr_repeat_flag { get; set; }
+        public Boolean? srr_repeat_flag { get; set; }
     }
 }
